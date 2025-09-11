93eecd3bf210595e4bc6db362b2ee50049931fcb
Qbertf <christopher.nolan.ytb0@gmail.com>
2025-09-11 12:11:40 +0330

    Update Sam2Infer_v2.py

--- Sam2Infer_v2.py
+++ Sam2Infer_v2.py
@@ -1,3 +1,4 @@
+%%writefile football/Sam2Infer_v3.py 
 import argparse
 import sys
 import os
@@ -64,34 +65,78 @@ class Sam2Infer:
       self.Save_Video = args.Save_Video
       self.Save_Segs = args.Save_Segs
       self.Own_ball = args.Own_ball
-  def run(self):
+      self.last_episode =  args.last_episode
+      self.OVERLAP = args.OVERLAP
       
+  def run(self):
+
+      listball_f=None
       ## 1. ball tracking
       listball_f,video_segments_brd = self.ball_detection()
 
+      flg_ep=False
       ## 2. player + ball tracking
-      best_det = self.listdetect[0];frame_index = 0;
-      best_det = trackutils.valid_det(best_det,self.listframes[frame_index])
-      video_segments,qball = self.playsam(self.video_dir+'/',best_det,frame_index,listball_f,showfig=False,qball=99)
 
+      partnumber = int(self.video_dir.split('/')[-1].replace('part_',''))
+      print('partnumber',partnumber)
+      if partnumber>1:
+          flg_ep,video_segments,qball = self.playsamC(self.video_dir+'/',best_det=None,frame_index=None,\
+                                                      listball_f=listball_f,partnumber=partnumber,showfig=True,qball=99)
+      else:
+          flg_ep = True
+          
+      if flg_ep==True:
+          best_det = self.listdetect[0];frame_index = 0;
+          best_det = trackutils.valid_det(best_det,self.listframes[frame_index])
+          video_segments,qball = self.playsam(self.video_dir+'/',best_det,frame_index,listball_f,showfig=False,qball=99)
+
+      
+      
       ## 3. reverse tracking
       bx = np.sort(list(video_segments.keys()))[-1]
       det1 = self.listdetect[-1];det1 = trackutils.valid_det(det1,self.listframes[-1])
       det2 = video_segments[bx]
       detc = trackutils.find_empty(det1,det2,qball=99)
 
+      #with open('/kaggle/working/temp.pkl', 'wb') as fb:
+      #    pickle.dump({'video_dir':self.video_dir,'video_segments':video_segments,'listdetect':self.listdetect,'listpath':self.listpath,'listframes':self.listframes}, fb)
+    
+      #print(asd)
+      
       if detc is not None:
           trackutils.reverse_and_rename_images(self.video_dir+'/', 'temp_r')
           video_segments_r,_ = self.playsam('temp_r/',detc,0,listball_f=None,showfig=False,qball=99)
           video_segments_r_reversed = trackutils.reverse_dict_values_by_order_new(video_segments_r,len(self.listframes))
-          video_segments_r_reversed_n = trackutils.add_100_to_inner_keys(video_segments_r_reversed)
+          video_segments_r_reversed_n = trackutils.add_100_to_inner_keys(video_segments_r_reversed,number=partnumber*100)
           video_segments = trackutils.merge_nested_dicts(video_segments, video_segments_r_reversed_n)
-          video_segments = trackutils.filter_objects_with_high_iou(video_segments,qball)   
+          video_segments = trackutils.filter_objects_with_high_iou(video_segments,qball,number=partnumber*100)   
           os.system('rm -rf temp_r')
 
           if len(video_segments_brd)!=0:
               video_segments = trackutils.merge_nested_dicts(video_segments, video_segments_brd)
 
+      '''
+      video_segments_ex_newid = video_segments.copy()
+      mydata={}
+      for hh in range(0,10):
+          for index in range(0,len(self.listdetect)):
+              det1 = self.listdetect[index]
+              det1 = trackutils.valid_det(det1,self.listframes[index])
+              if index in video_segments_ex_newid.keys():
+                  det2 = video_segments_ex_newid[index]
+                  detc = trackutils.find_empty(det1,det2,qball=99)
+                  if detc is not None:
+                      foldertemp = 'temp_r'
+                      os.system('mkdir -p '+foldertemp)
+                      video_segments_ex,_ = self.playsam(self.video_dir+'/',detc,index,listball_f=None,showfig=False,qball=99)
+                      video_segments_ex_newid = trackutils.add_100_to_inner_keys(video_segments_ex,number=(hh+2)*100)
+                      mydata.update({hh:[video_segments_ex_newid,detc]})
+                      break
+      '''
+      #with open('/kaggle/working/temp.pkl', 'wb') as fb:
+      #    pickle.dump({'video_dir':self.video_dir,'video_segments':video_segments,'listdetect':self.listdetect,'listpath':self.listpath,'listframes':self.listframes}, fb)
+
+      #print(asd)
       video_segments = self.area_verify(video_segments)
       player_balls = None;
       if self.Own_ball=='True':
@@ -103,7 +148,7 @@ class Sam2Infer:
       #    pickle.dump({'video_dir':self.video_dir,'video_segments':video_segments,'listpath':self.listpath}, fb)
       if self.Save_Segs=='True':
         with gzip.open(self.video_dir.replace('/','_')+'_track.pkl.gz', 'wb') as fb:
-            pickle.dump({'video_dir':self.video_dir,'video_segments':video_segments,'player_balls':player_balls,'listpath':self.listpath}, fb)
+            pickle.dump({'video_dir':self.video_dir,'video_segments':video_segments,'player_balls':player_balls,'listpath':self.listpath,'listframes':self.listframes}, fb)
       else:
         with open(self.video_dir.replace('/','_')+'_track.pkl', 'wb') as fb:
           pickle.dump({'video_dir':self.video_dir,'video_segments':self.convertsegs(video_segments),'player_balls':player_balls,'listpath':self.listpath}, fb)
@@ -191,6 +236,109 @@ class Sam2Infer:
           model.eval()    
       return model
 
+  def playsamD(self,video_dir,best_det,frame_index,listball_f=None,showfig=True,qball=99,outpath=''):
+      os.system('python football/Sam2Direct.py --video_dir '+video_dir\
+                +' --best_det '+str(best_det)+ ' --frame_index '+frame_index +' --listball_f '+str(listball_f) +' --outpath '+outpath)
+
+      with open(outpath) as fb:
+          data = pickle.load(fb)
+
+      return data['video_segments'],data['qball']
+
+  def playsamC(self,video_dir,best_det=None,frame_index=None,listball_f=None,partnumber=1,showfig=True,qball=99):
+
+        if self.last_episode!='_':
+            if os.path.exists(self.last_episode):
+                print('last_episode',self.last_episode)
+                with open(self.last_episode, 'rb') as fb:
+                    last_data = pickle.load(fb)
+                video_segments_last = last_data['video_segments'] 
+                listpath_last = last_data['listpath'] 
+                #print('listpath_last',listpath_last)
+                #print(self.OVERLAP)
+                #print(asd)
+            else:
+                flg_ep=True
+                return flg_ep,None,qball
+        else:
+            flg_ep=True
+            return flg_ep,None,qball
+
+        
+        currlist=[]
+        for i in range(len(self.listpath)):
+            currlist.append(self.listpath[i].split('/')[-1])
+            
+        frame_names = [
+            p for p in os.listdir(video_dir)
+            if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+        ]
+        frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
+        
+        predictor = self.build_sam2_video_predictor_naked(
+            yaml_path=self.model_cfg,
+            ckpt_path=self.sam2_checkpoint,
+            device="cuda"
+        )
+        inference_state = predictor.init_state(video_path=video_dir)
+        predictor.reset_state(inference_state)
+                              
+        ln = len(listpath_last)
+        focus = listpath_last[ln-self.OVERLAP:]
+        #print('focus',focus)
+        #print('currlist',currlist[self.OVERLAP:])
+        #print('partname',partnumber)
+        idn=[]
+        for hp in range(ln-self.OVERLAP,len(listpath_last)):
+            idn.append(hp)
+        #print('idn',idn)
+        yup = 0
+        for fpath in focus:
+            fname = fpath.split('/')[-1]
+            #frame_index = int(fpath.split('/')[-1].replace('.jpg',''))-1
+            frame_index = idn[yup]
+            yup+=1
+            #print('frame_index',frame_index)
+            if frame_index in video_segments_last.keys():
+                best_det = video_segments_last[frame_index]
+                if len(best_det)>=2:
+                    if len(best_det['xyxy'])>=1:
+                        for q,box in enumerate(best_det['xyxy']):
+                            tracker_id  = best_det['tracker_id'][q]
+                            box = np.asarray(box,dtype=np.float32)
+                            
+                            fidx = currlist.index(fname)
+                            #print('box',box,tracker_id,fidx)
+                            _, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+                            inference_state=inference_state,
+                            frame_idx=fidx,
+                            obj_id=tracker_id,
+                            box=box,)
+
+        #print(asd)
+        if listball_f is not None:
+            for balls in listball_f:
+                box,fq = balls
+                box = np.asarray(box.xyxy[0],dtype=np.float32)
+                _, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+                  inference_state=inference_state,
+                  frame_idx=fq,
+                  obj_id=qball,
+                  box=box,)
+        else:
+            q=-1;
+
+        #print('playsamCCCCCCCCCC',video_dir)
+        video_segments = {}  
+        for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state):
+            video_segments[out_frame_idx] = {
+                out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
+                for i, out_obj_id in enumerate(out_obj_ids)
+            }
+    
+        del predictor
+        return False,video_segments,qball
+            
   def playsam(self,video_dir,best_det,frame_index,listball_f=None,showfig=True,qball=99):
         frame_names = [
             p for p in os.listdir(video_dir)
@@ -271,6 +419,9 @@ if __name__ == "__main__":
     parser.add_argument("--Save_Video", type=str, default='True', help="save path")
     parser.add_argument("--Save_Segs", type=str, default='True', help="save path")
     parser.add_argument("--Own_ball", type=str, default='True', help="save path")
+    parser.add_argument("--last_episode", type=str, default='True', help="save path")
+    parser.add_argument("--OVERLAP", type=int, default='True', help="save path")
+
 
     args = parser.parse_args()
     sys.path.append(args.sam2_PathLib)
