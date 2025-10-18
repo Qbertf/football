90dce8c3a59977584f8e4f693b63cd9170c26056
Qbertf <christopher.nolan.ytb0@gmail.com>
2025-10-18 14:16:17 +0330

    Add files via upload

---
+++ other/calibration.py
@@ -0,0 +1,205 @@
+import numpy as np
+from Calibration import similar
+import pickle
+from Calibration import view
+import glob
+import cv2
+import os
+import re
+
+class Calibration:
+    def __init__(self,match_path):
+        self.soccer_coords_ref = {1: (0.00, 0.00),2: (105.00, 0.00),3: (105.00, 68.00),4: (0.00, 68.00),5: (16.50, 13.84),6: (0.0, 13.84),7: (0.00, 54.16),8: (16.50, 54.16),9: (88.50, 13.84),10: (105.00, 13.84),11: (105.00, 54.16),12: (88.50, 54.16),13: (5.50, 24.84),14: (0.00, 24.84),15: (0.00, 43.16),16: (5.50, 43.16),17: (99.50, 24.84),18: (105.00, 24.84),19: (105.00, 43.16),20: (99.50, 43.16),21: (52.50, 34.00),22: (52.50, 68.00),23: (52.50, 0.00),24: (52.50, 43.15),25: (52.50, 24.85),26: (43.35, 34.00),27: (61.65, 34.00),28: (16.63, 26.79),29: (16.63, 41.21),30: (88.37, 41.21),31: (88.37, 26.79),32: (11.00, 34.00),33: (94.00, 34.00),34:(0.00, 30.34) , 35 : (0.00, 37.66), 36:(105.00, 30.34), 37:(105.00, 37.66)}
+        
+        soccer_coords_array=[]
+        for key in self.soccer_coords_ref.keys():
+            soccer_coords_array.append(self.soccer_coords_ref[key])
+        self.soccer_coords_array = np.asarray(soccer_coords_array)
+
+        self.MATCH_PATH = match_path
+
+    def validation_operator_file(self):
+        TP=0;FP=0;
+        self.operator_calibration_file_validate={}
+        for key in self.operator_calibration_file.keys():
+            MAPPoints = self.operator_calibration_file[key]['points']
+            image_pixels = []
+            soccer_coords = []
+            for item in MAPPoints:
+                image_pixels.append([item['imagePixel']['x'], item['imagePixel']['y']])
+                soccer_coords.append([item['soccerPoint']['x'], item['soccerPoint']['y']])
+
+            if len(np.asarray(image_pixels)) >= 4:
+                transformer_image2radar = view.ViewTransformer(source=np.asarray(image_pixels), target=np.asarray(soccer_coords))
+
+                transformer_radar2image = view.ViewTransformer(
+                    source=np.asarray(soccer_coords),
+                    target=np.asarray(image_pixels)
+                )
+            
+                frame_all_points = transformer_radar2image.transform_points(points=self.soccer_coords_array)
+                total_valid_points = len(np.unique(frame_all_points))
+
+                if total_valid_points>=8:
+                    self.operator_calibration_file_validate.update({key:{'transformer_image2radar':transformer_image2radar,'transformer_radar2image':transformer_radar2image,'points':MAPPoints}})
+                    TP+=1;
+                else:
+                    FP+=1;
+                    #pass
+                    #print('not valid --> ', key,total_valid_points)
+                    #print('-----------------')
+
+        print('TP '+str(TP) + ' FP '+str(FP) + ' | R '+str(TP*100/(TP+FP))[:4]+'%' )
+
+    def load_file_operator(self,path):
+
+        if os.path.exists(path):
+            with open(path,'rb') as f:
+                self.operator_calibration_file = pickle.load(f)
+                self.validation_operator_file()
+                self.load_ref_image(windows=0)
+                self.gathar_all_query_image(windows=0)
+
+                return self.refsImage,self.query_image_path,self.operator_calibration_file_validate,self.operator_calibration_file
+
+        else:
+            raise("operator_calibration_file not exists")
+
+
+    def load_ref_image(self,windows=1):
+
+        self.refsImage={}
+        for key in self.operator_calibration_file_validate.keys():
+            path = self.MATCH_PATH+key
+            if windows==1:
+                path = re.sub(r'/part_\d+/', '/', path)
+            #path = path.replace('part_0002','part_0001').replace('part_0003','part_0001')
+            if os.path.exists(path):
+                self.refsImage.update({key:cv2.imread(path)})
+            #else:
+                #print(path)
+
+    def gathar_all_query_image(self,windows=1):
+        self.query_image_path=[]
+        if windows==1:
+            paths = glob.glob(self.MATCH_PATH+'epi*/*.jpg')
+        else:
+            paths = glob.glob(self.MATCH_PATH+'epi*/part*/*.jpg')
+            
+        for path in np.sort(paths):
+            path = path.replace('\\','/')
+            self.query_image_path.append(path)
+        
+
+    def propagete_calibration(self,method="gray",limit=None):
+
+        if method=="gray":
+            self.pair_socre = similar.base_gray_color(self.query_image_path,self.operator_calibration_file_validate,self.MATCH_PATH,self.refsImage)
+
+        elif method=="tm":
+            self.pair_socre = similar.base_tm(self.query_image_path,self.operator_calibration_file_validate,self.MATCH_PATH,self.refsImage,limit)
+
+        elif method=="ssim":
+            self.pair_socre = similar.base_ssim(self.query_image_path,self.operator_calibration_file_validate,self.MATCH_PATH,self.refsImage)
+
+        elif method=="sift":
+            self.pair_socre = similar.base_sift(self.query_image_path,self.operator_calibration_file_validate,self.MATCH_PATH,self.refsImage)
+
+        elif method=="some":
+            self.pair_socre = similar.base_some_color(self.query_image_path,self.operator_calibration_file_validate,self.MATCH_PATH,self.refsImage)
+        
+
+        return self.pair_socre
+
+
+    def direct_solution(self,CONFIG,method="tm",limit=None):
+        self.query_image_path = []
+        paths = glob.glob("/root/console/"+self.MATCH_PATH+'epi*/part*/*.jpg')
+        for path in np.sort(paths):
+            path = path.replace('\\','/')
+            self.query_image_path.append(path)
+            
+        
+        self.refsImage = {}
+
+        for full_folder_path in np.sort(glob.glob(self.MATCH_PATH+"epi*/part*")):
+        
+            allpathsort = np.sort(glob.glob(full_folder_path+'/*.jpg'))
+    
+            if 'episode_Z' not in full_folder_path:
+                if len(allpathsort)>=3:
+                    A=allpathsort[0]
+                    C=allpathsort[-1]
+                    if len(allpathsort)%2==0:
+                        B=int(np.ceil((len(allpathsort))/2));B=allpathsort[B]
+                    else:
+                        B=int(np.ceil((len(allpathsort)-1)/2));B=allpathsort[B]
+                    allpathsort=[];
+                    allpathsort.append(A);allpathsort.append(B);allpathsort.append(C);
+                    allpathsort=np.asarray(allpathsort)
+
+                #print('AAAA',allpathsort)
+                for path in allpathsort:
+                    key = path.replace(self.MATCH_PATH,"")
+                    self.refsImage.update({key:cv2.imread(path)})
+
+                    
+        if method=="tm":
+            #print('self.query_image_path',self.query_image_path)
+            self.pair_socre = similar.base_tm(self.query_image_path,None,self.MATCH_PATH,self.refsImage,limit)
+            return self.pair_socre
+
+    def draw_circle(self,image,center_point):
+        #center_point = center_point
+        center_point= (int(center_point[0]), int(center_point[1]))
+        # شعاع دایره
+        radius = 5
+        # رنگ دایره (BGR format)
+        color = (255, 0, 0)  # سبز
+        # ضخامت خط (اگر منفی باشد دایره پر میشود)
+        thickness = -1
+        # کشیدن دایره روی تصویر
+        cv2.circle(image, center_point, radius, color, thickness)
+        return image
+
+    def draw_pitch_on_image(self,image,H):
+
+        for key in self.soccer_coords_ref.keys():
+            points = np.asarray(self.soccer_coords_ref[key])
+            map1 = view.rawTransofrm(points,H)
+
+            image = self.draw_circle(image,(map1[0,0],map1[0,1]))
+
+        return image
+
+    def add_text_below_image(self,image, text, background_color=(0, 0, 0), text_color=(255, 255, 255)):
+        
+        # دریافت ابعاد تصویر اصلی
+        height, width = image.shape[:2]
+        
+        # تعیین اندازه فونت و ضخامت متن
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.8
+        thickness = 2
+        
+        # محاسبه اندازه متن
+        (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+        
+        # ایجاد ناحیه سیاه برای متن (20 پیکسل بیشتر از ارتفاع متن برای حاشیه)
+        text_area_height = text_height + 40
+        
+        # ایجاد تصویر سیاه برای ناحیه متن
+        text_area = np.zeros((text_area_height, width, 3), dtype=np.uint8)
+        text_area[:] = background_color
+        
+        # قرار دادن متن در مرکز ناحیه سیاه
+        text_x = (width - text_width) // 2
+        text_y = (text_area_height + text_height) // 2
+        
+        cv2.putText(text_area, text, (text_x, text_y), font, font_scale, text_color, thickness)
+        
+        # ترکیب تصویر اصلی با ناحیه متن
+        result = np.vstack((image, text_area))
+        
+        return result
+        
---
+++ other/similar.py
@@ -0,0 +1,367 @@
+from tqdm import tqdm
+import numpy as np
+import cv2
+import re
+import os
+from skimage.metrics import structural_similarity as ssim
+import sys
+
+def base_gray_color(paths,operator_calibration_file_validate,MATCH_PATH,refsImage):
+    
+    pair_socre={}
+    q=0;
+    for path in tqdm(paths[:30]):
+        query_frame = cv2.imread(path,0)
+        r=0;
+        for keyref in refsImage.keys():
+            ref_image = cv2.cvtColor(refsImage[keyref], cv2.COLOR_BGR2GRAY)
+            pair_key = (path,keyref)
+            score = np.sum(query_frame - ref_image)                      
+            pair_socre.update({pair_key:score})
+            r+=1;
+        q+=1;
+
+    return pair_socre
+
+def base_some_color(paths,operator_calibration_file_validate,MATCH_PATH,refsImage):
+    
+    pair_socre={}
+    q=0;
+    for path in tqdm(paths[:1000]):
+        query_frame = cv2.imread(path)
+        query_frame = cv2.cvtColor(query_frame, cv2.COLOR_BGR2LAB)[1:3]
+
+        r=0;
+        for keyref in refsImage.keys():
+            ref_image = cv2.cvtColor(refsImage[keyref], cv2.COLOR_BGR2LAB)[1:3]
+            pair_key = (path,keyref)
+            score = np.mean(abs(query_frame - ref_image))                  
+            pair_socre.update({pair_key:score})
+            r+=1;
+        q+=1;
+
+    return pair_socre
+
+def base_tm(paths,operator_calibration_file_validate,MATCH_PATH,refsImage,limit=None):
+
+    class Tee:
+        def __init__(self, *files):
+            self.files = files
+        def write(self, obj):
+            for f in self.files:
+                f.write(obj)
+                f.flush()
+        def flush(self):
+            for f in self.files:
+                f.flush()
+    
+    #with open("log_calib.txt", "w") as f:
+    #  f.write("")
+    log_file = open("log_calib.txt", "w", buffering=1)
+    log_file.write("RUNCALIB PAIRSCORE\n")
+    
+    log_file.write("REF: " + MATCH_PATH + " ---> " + str(len(refsImage)) + " QUERY: " +  str(len(paths))  +"\n")
+   
+    
+    sys.stdout = Tee(sys.stdout, log_file)
+    sys.stderr = Tee(sys.stderr, log_file)  # tqdm uses stderr by default
+    
+    pair_socre={}
+    q=0;
+    if limit is not None:
+        paths=paths[:limit]
+    for path in tqdm(paths):
+        query_frame = cv2.imread(path,0)
+        r=0;
+        for keyref in refsImage.keys():
+            ref_image = cv2.cvtColor(refsImage[keyref], cv2.COLOR_BGR2GRAY)
+            pair_key = (path,keyref)
+            score = template_matching(query_frame, ref_image)                    
+            pair_socre.update({pair_key:score})
+            r+=1;
+        q+=1;
+
+    return pair_socre
+
+def base_ssim(paths,operator_calibration_file_validate,MATCH_PATH,refsImage):
+    
+    pair_socre={}
+    q=0;
+    for path in tqdm(paths[:30]):
+        query_frame = cv2.imread(path,0)
+        r=0;
+        for keyref in refsImage.keys():
+            ref_image = cv2.cvtColor(refsImage[keyref], cv2.COLOR_BGR2GRAY)
+            pair_key = (path,keyref)
+            score, _ = ssim(ref_image, query_frame, full=True)                    
+            pair_socre.update({pair_key:score})
+            r+=1;
+        q+=1;
+
+    return pair_socre
+
+def base_sift(paths,operator_calibration_file_validate,MATCH_PATH,refsImage):
+    
+    pair_socre={}
+    q=0;
+    for path in tqdm(paths[:30]):
+        query_frame = cv2.imread(path,0)
+        r=0;
+        for keyref in refsImage.keys():
+            ref_image = cv2.cvtColor(refsImage[keyref], cv2.COLOR_BGR2GRAY)
+            pair_key = (path,keyref)
+            score = feature_matching_fast(ref_image, query_frame)                  
+            pair_socre.update({pair_key:score})
+            r+=1;
+        q+=1;
+
+    return pair_socre
+
+def feature_matching_fast(img1, img2, max_features=1000, ratio_test=0.75):
+    """
+    نسخه بهینه‌شده تطابق ویژگی‌ها با سرعت بالاتر
+    
+    Parameters:
+    - img1, img2: تصاویر ورودی
+    - max_features: حداکثر تعداد ویژگی‌های استخراج شده
+    - ratio_test: آستانه برای فیلتر کردن تطابق‌های ضعیف
+    """
+    
+    # ایجاد detector ORB با محدودیت تعداد ویژگی‌ها
+    orb = cv2.ORB_create(nfeatures=max_features)
+    
+    # یافتن keypoints و descriptors
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    
+    # اگر تعداد ویژگی‌ها کم باشد، امتیاز صفر برگردان
+    if des1 is None or des2 is None or len(des1) < 4 or len(des2) < 4:
+        return 0.0
+    
+    # استفاده از FLANN-based matcher برای سرعت بیشتر
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm=FLANN_INDEX_LSH,
+                       table_number=6,
+                       key_size=12,
+                       multi_probe_level=1)
+    search_params = dict(checks=50)
+    
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    
+    # تطابق با روش KNN (سرعت بیشتر + دقت بهتر)
+    matches = flann.knnMatch(des1, des2, k=2)
+    
+    # فیلتر کردن تطابق‌های خوب با استفاده از ratio test
+    good_matches = []
+    for match_pair in matches:
+        if len(match_pair) == 2:
+            m, n = match_pair
+            if m.distance < ratio_test * n.distance:
+                good_matches.append(m)
+    
+    # محاسبه امتیاز شباهت
+    min_features = min(len(kp1), len(kp2))
+    if min_features == 0:
+        return 0.0
+    
+    similarity = len(good_matches) / min_features
+    return similarity
+
+def feature_matching(img1, img2):
+   
+    # ایجاد detector ORB
+    orb = cv2.ORB_create()
+    
+    # یافتن keypoints و descriptors
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    
+    # ایجاد matcher
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    
+    # تطابق ویژگی‌ها
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    
+    # محاسبه امتیاز شباهت
+    similarity = len(matches) / min(len(kp1), len(kp2))
+    return similarity
+
+def template_matching(query, ref,resize_factor=0.5):
+
+    query = cv2.resize(query, None, fx=resize_factor, fy=resize_factor)
+    ref = cv2.resize(ref, None, fx=resize_factor, fy=resize_factor)
+
+    result = cv2.matchTemplate(ref, query, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    
+    return max_val  #最高 میزان شباهت
+
+def calculate_homography_between_frames(image1,image2):
+ 
+    #image1 = cv2.imread(path1)
+    #image2 = cv2.imread(path2)
+
+    new_mask1 = np.zeros_like(image1)[:,:,0]
+    new_mask1[:140,:220]=1;new_mask1[:140,1170:]=1;
+    new_mask2=new_mask1.copy()
+    
+    #print('new_mask1',new_mask1.shape)
+
+    img1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
+
+    sift = cv2.SIFT_create()
+    keypoints1, descriptors1 = sift.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    matches = bf.match(descriptors1, descriptors2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    
+    pts1 = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    pts2 = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+    distance, angle_deg , _ = calculate_distance_and_angle(pts1, pts2)
+    tps = np.where(np.asarray(distance)<=50)
+    npts1=pts1[tps];npts2=pts2[tps];
+
+    #if domask==True:
+        #if new_mask1 is not None and 
+
+    pts1_coords = npts1[:, 0, :].astype(int)
+    pts2_coords = npts2[:, 0, :].astype(int)
+    valid_mask1 = new_mask1[pts1_coords[:, 1], pts1_coords[:, 0]] == 0
+    valid_mask2 = new_mask2[pts2_coords[:, 1], pts2_coords[:, 0]] == 0  
+    valid_mask = valid_mask1 & valid_mask2
+    npts1 = npts1[valid_mask]
+    npts2 = npts2[valid_mask]
+
+    H, mask = cv2.findHomography(npts1, npts2, cv2.RANSAC, 5)
+    H_INV, mask = cv2.findHomography(npts2, npts1, cv2.RANSAC,5)
+
+    return H,H_INV
+
+from lightglue import LightGlue, SuperPoint, DISK
+from lightglue.utils import load_image, rbd
+from lightglue import viz2d
+import torch
+
+def calculate_homography_between_frames_novel(image1,image2,device,extractor,matcher):
+ 
+    feats0 = extractor.extract(image1.to(device))
+    feats1 = extractor.extract(image2.to(device))
+    matches01 = matcher({"image0": feats0, "image1": feats1})
+    feats0, feats1, matches01 = [
+        rbd(x) for x in [feats0, feats1, matches01]
+    ]  # remove batch dimension
+
+    kpts0, kpts1, matches = feats0["keypoints"], feats1["keypoints"], matches01["matches"]
+    m_kpts0, m_kpts1 = kpts0[matches[..., 0]], kpts1[matches[..., 1]]
+
+    # تبدیل تصاویر به فرمت مناسب (Channels-Last)
+    if isinstance(image1, torch.Tensor):
+        image1 = image1.cpu().numpy()
+    if isinstance(image2, torch.Tensor):
+        image2 = image2.cpu().numpy()
+    
+    # اگر تصاویر به شکل (C, H, W) باشند، به (H, W, C) تبدیل می‌کنیم
+    if image1.shape[0] == 3:  # Channels-First
+        image1 = np.transpose(image1, (1, 2, 0))
+    if image2.shape[0] == 3:  # Channels-First
+        image2 = np.transpose(image2, (1, 2, 0))
+    
+    # اطمینان از اینکه تصاویر uint8 هستند
+    if image1.dtype != np.uint8:
+        image1 = (image1 * 255).astype(np.uint8)
+    if image2.dtype != np.uint8:
+        image2 = (image2 * 255).astype(np.uint8)
+    
+    # اگر تصویر grayscale است، به RGB تبدیل کنید
+    if len(image1.shape) == 2:
+        image1 = cv2.cvtColor(image1, cv2.COLOR_GRAY2BGR)
+    if len(image2.shape) == 2:
+        image2 = cv2.cvtColor(image2, cv2.COLOR_GRAY2BGR)
+
+    # تعریف ماسک‌ها بر اساس شکل تصویر
+    new_mask1 = np.zeros((image1.shape[0], image1.shape[1]), dtype=np.uint8)
+    new_mask1[:140, :220] = 1
+    new_mask1[:140, 1170:] = 1
+    #new_mask1[200:, :] = 1
+    new_mask2 = new_mask1.copy()
+
+    # تبدیل keypointها به numpy
+    npts0 = m_kpts0.cpu().numpy().astype(np.float32) if hasattr(m_kpts0, 'cpu') else m_kpts0.astype(np.float32)
+    npts1 = m_kpts1.cpu().numpy().astype(np.float32) if hasattr(m_kpts1, 'cpu') else m_kpts1.astype(np.float32)
+
+    # فیلتر کردن keypointها بر اساس ماسک
+    pts0_coords = npts0.astype(int)
+    pts1_coords = npts1.astype(int)
+
+    # اطمینان از اینکه مختصات در محدوده تصویر باشند
+    height, width = new_mask1.shape[:2]
+    pts0_coords[:, 0] = np.clip(pts0_coords[:, 0], 0, width-1)
+    pts0_coords[:, 1] = np.clip(pts0_coords[:, 1], 0, height-1)
+    pts1_coords[:, 0] = np.clip(pts1_coords[:, 0], 0, width-1)
+    pts1_coords[:, 1] = np.clip(pts1_coords[:, 1], 0, height-1)
+
+    # اعمال ماسک
+    valid_mask0 = new_mask1[pts0_coords[:, 1], pts0_coords[:, 0]] == 0
+    valid_mask1 = new_mask2[pts1_coords[:, 1], pts1_coords[:, 0]] == 0
+    valid_mask = valid_mask0 & valid_mask1
+
+    # فیلتر کردن keypointهای معتبر
+    npts0 = npts0[valid_mask]
+    npts1 = npts1[valid_mask]
+
+    H, mask = cv2.findHomography(npts0, npts1, cv2.RANSAC, 5)
+    H_INV, mask = cv2.findHomography(npts1, npts0, cv2.RANSAC,5)
+
+    #print('H',H)
+    #print('H_INV',H_INV)
+    return H,H_INV
+    
+    
+import cv2
+import numpy as np
+import math
+
+def calculate_distance_and_angle(pts1, pts2):
+    """
+    Calculate distance and angle between corresponding points
+    
+    Parameters:
+    pts1, pts2: numpy arrays of shape (N, 1, 2) containing corresponding points
+    
+    Returns:
+    distances: list of Euclidean distances between corresponding points
+    angles: list of angles in degrees between corresponding points
+    vectors: list of displacement vectors (dx, dy)
+    """
+    
+    # Ensure points are in correct shape
+    pts1 = pts1.reshape(-1, 2)
+    pts2 = pts2.reshape(-1, 2)
+    
+    distances = []
+    angles = []
+    vectors = []
+    
+    for p1, p2 in zip(pts1, pts2):
+        # Calculate displacement vector
+        dx = p2[0] - p1[0]
+        dy = p2[1] - p1[1]
+        
+        # Calculate Euclidean distance
+        dist = math.sqrt(dx**2 + dy**2)
+        
+        # Calculate angle in degrees (0-360)
+        angle_rad = math.atan2(dy, dx)
+        angle_deg = math.degrees(angle_rad) % 360
+        
+        distances.append(dist)
+        angles.append(angle_deg)
+        vectors.append((dx, dy))
+    
+    return distances, angles, vectors
